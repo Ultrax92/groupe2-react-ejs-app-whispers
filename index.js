a7cf24c852af317061d2dd50d1c6c25bf537cbde
Ultrax92 <kiwiihs@gmail.com>
2025-06-20 11:53:44 +0200

    Update port

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { app } from "./server.js";
-const port = 3000;
+// const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Running in http://localhost:${port}`);
 });
